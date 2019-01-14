b8d42dc9b9b6c54b0d3f1f58f19ca14c89976c1e
mutsune <dev@mutsune.com>
2019-01-14 19:00:11 +0900

    fix

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { parse } = require('url')
+// const { parse } = require('url')
 
 module.exports = (req, res) => {
-  const { query } = parse(req.url, true)
-  const { name = 'World' } = query
+  // const { query } = parse(req.url, true)
+  // const { name = 'World' } = query
+  console.log("log test");
+  const name = "World";
   res.end(`Hello ${name}!`)
 }
